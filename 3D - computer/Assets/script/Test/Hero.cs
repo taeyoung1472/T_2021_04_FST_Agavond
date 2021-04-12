19c843d76ab465c0c63763dfc494dc1e55fa7b4c
taeyoung1472 <77605585+taeyoung1472@users.noreply.github.com>
2021-04-13 08:38:08 +0900

    2021-4-13

--- 3D - computer/Assets/script/Test/Hero.cs
+++ 3D - computer/Assets/script/Test/Hero.cs
@@ -6,13 +6,26 @@ public class Hero : MonoBehaviour
 {
     [SerializeField]
     CharacterController cc;
-
+    public float basicspeed;
     public float speed = 3.0F;
+    public float runspeed;
     public float jumpSpeed = 4.0F;
     public float gravity = 9.81F;
     private Vector3 moveDirection = Vector3.zero;
+    private void Start()
+    {
+        basicspeed = speed;
+    }
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runspeed;
+        }
+        else
+        {
+            speed = basicspeed;
+        }
         if (cc.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
