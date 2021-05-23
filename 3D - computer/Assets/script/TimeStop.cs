f132e3dee80a634daa0e7caaf61e41d2d2108afa
taeyoung1472 <77605585+taeyoung1472@users.noreply.github.com>
2021-05-24 00:41:56 +0900

    이태영
    정지버튼-돌아가기버튼

---
+++ 3D - computer/Assets/script/TimeStop.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class TimeStop : MonoBehaviour
+{
+    public bool isStop;
+    public GameObject panel;
+    public GameObject main;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isStop == true)
+        {
+            Time.timeScale = 0;
+        }
+        if(isStop == false)
+        {
+            Time.timeScale = 1;
+        }
+    }
+    public void Stop()
+    {
+        isStop = true;
+        panel.SetActive(true);
+    }
+    public void GO()
+    {
+        isStop = false;
+        panel.SetActive(false);
+    }
+    public void GiveUp()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
